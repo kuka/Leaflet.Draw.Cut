d788e77462c2c15b09898ca4365e0fcdf648b426
Lucas Sotty <lucas.sotty@wanadoo.fr>
2018-02-01 17:59:49 +0100

    Trying to setting things up

---
+++ spec/DrawCutPolylineSpec.js
@@ -0,0 +1,25 @@
+// var L = require('leaflet');
+
+// describe('DrawCutPolyline', function() {
+//   var polygon;
+//   var latlngs;
+//   var test;
+
+//   beforeEach(function() {
+//     this.test = 'test';
+//     this.latlngs = [[37, -109.05],[41, -109.03],[41, -102.05],[37, -102.04]];
+//     this.polygon = new L.Polygon(this.latlngs);
+//   });
+
+//   it('blabla', function() {
+//     debugger;
+//   });
+// });
+
+describe('test', function() {
+
+  it('blabla', function() {
+    var addition = window.addition(4, 6);
+    expect(addition).toEqual(10);
+  });
+});
